3b356a5a2d80d26d19aa01c49111d9c8c312f9a3
Divyank Agarwal <divagar@iu.edu>
2023-03-23 15:30:10 -0400

    Intitial Commit

---
+++ src/main/java/ecc/project/community/connect/CommunityConnectApplication.java
@@ -0,0 +1,13 @@
+package ecc.project.community.connect;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CommunityConnectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CommunityConnectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ecc/project/community/connect/CommunityConnectApplicationTests.java
@@ -0,0 +1,13 @@
+package ecc.project.community.connect;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CommunityConnectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
