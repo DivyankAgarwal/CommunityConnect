cc1118e71fad2e58e4549856b88b47fffabb8759
Mrunal Patil <mrupatil@users.noreply.github.com>
2023-03-23 15:59:04 -0400

    Add simple controller

---
+++ src/main/java/ecc/project/community/connect/controller/MainController.java
@@ -0,0 +1,13 @@
+package ecc.project.community.connect.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class MainController {
+
+    @GetMapping("/hello")
+    public String hello() {
+        return "Hello World";
+    }
+}
